4e6d0b4caf1b5700291c2ba8952c903dab653da8
razeeth11 <razeeth011@gmail.com>
2023-03-05 22:42:04 +0530

    Username&pass

--- router/users.router.js
+++ router/users.router.js
@@ -1,22 +1,48 @@
 import express from "express";
 const router = express.Router();
-import { createUser } from "../service/users.service.js";
+import { createUser,getUserByName,getUserName } from "../service/users.service.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 
 
 router.post("/signup", async function (request, response) {
 
   const {username,password} = request.body;
   const noOfRounds = 5;
-  const salt = await bcrypt.genSalt(noOfRounds);
-  const hashPass = await bcrypt.hash(password, salt);
+  const getByName = await getUserByName(username);
 
-  const usersData = await createUser({
-    username : username,
-    password : hashPass
-  });
-    
-  response.send(usersData)
+  if(getByName){
+      response.status(404).send({message : "username is already exist"})
+  } else {
+    const salt = await bcrypt.genSalt(noOfRounds);
+    const hashPass = await bcrypt.hash(password, salt);
+  
+    const usersData = await createUser({
+      username : username,
+      password : hashPass
+    });
+      
+    response.send(usersData)
+  }
+});
+
+router.post("/login", async function (request, response) {
+
+  const {username,password} = request.body;
+  const getUserByUsername = await getUserName(username);
+
+  if(!getUserByUsername){
+      response.status(400).send({message : "Invalid credentials"})
+  } else {
+    const pass = getUserByUsername.password
+    const og = await bcrypt.compare(password,pass)
+    if(og == true){
+      const token = jwt.sign({ id : getUserByUsername._id } , process.env.secret_key )
+      response.send({message : "Success" , token : token})
+    } else {
+      response.status(400).send({message : "Invalid credentials"})
+    }
+  }
 });
 
 export default router;
--- service/users.service.js
+++ service/users.service.js
@@ -4,3 +4,10 @@ export async function createUser(data) {
   return await client.db("moviesData").collection("users").insertOne(data)
 }
 
+export async function getUserByName(username) {
+  return await client.db("moviesData").collection("users").findOne({username : username})
+}
+
+export async function getUserName(username) {
+  return await client.db("moviesData").collection("users").findOne({username : username})
+}
